d70ce7a5e687a9a3af99436e8c19da6b6859f09a
Ayush Gupta <92868752+ayushgupta9906@users.noreply.github.com>
2024-06-15 13:12:40 +0530

    Time: 65 ms (58.67%) | Memory: 29 MB (51.08%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int target = 0;
-        sort(nums.begin(), nums.end());
-        set<vector<int>> s;
-        vector<vector<int>> output;
-        for (int i = 0; i < nums.size(); i++){
-            int j = i + 1;
-            int k = nums.size() - 1;
-            while (j < k) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+
+        sort(nums.begin() , nums.end());
+
+        for(int i=0; i<n; i++){
+            //same at start
+            if(i>0 && nums[i] == nums[i-1])  continue;
+            int j = i+1;
+            int k= n-1;
+            while(j < k){
                 int sum = nums[i] + nums[j] + nums[k];
-                if (sum == target) {
-                    s.insert({nums[i], nums[j], nums[k]});
+                if(sum < 0){
+                    j++;
+                }
+                else if(sum > 0){
+                    k--;
+                }
+                else{
+                    vector<int> temp = {nums[i] , nums[j] , nums[k]};
+                    ans.push_back(temp);
                     j++;
                     k--;
-                } else if (sum < target) {
-                    j++;
-                } else {
-                    k--;
+                    //for duplicate element subarray not repeat
+                    while(j<k && nums[j] == nums[j-1]) j++;
+                    while(j<k && nums[k] == nums[k+1]) k--;
                 }
             }
         }
-        for(auto triplets : s)
-            output.push_back(triplets);
-        return output;
+        return ans;
     }
 };
