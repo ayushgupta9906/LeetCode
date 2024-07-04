be2167d4e74d1beaf3dc032b6064ccb4ddbe9fe2
Ayush Gupta <92868752+ayushgupta9906@users.noreply.github.com>
2024-07-04 18:27:43 +0530

    Time: 27 ms (48.98%) | Memory: 94.8 MB (42.44%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -4,14 +4,11 @@ public:
         int c = v1.size() + v2.size();
         vector<int> merged(c);
 
-        // Merge the vectors using the merge function
         merge(v1.begin(), v1.end(), v2.begin(), v2.end(), merged.begin());
 
         if (c % 2 == 0) {
-            // If the combined size is even, return the average of the two middle elements
             return (merged[c/2 - 1] + merged[c/2]) / 2.0;
         } else {
-            // If the combined size is odd, return the middle element
             return merged[c/2];
         }
     }
