78868a02d07a8203f017c568ca00058bbef37cc7
Ayush Gupta <92868752+ayushgupta9906@users.noreply.github.com>
2024-07-06 19:55:56 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (90.21%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,22 +1,13 @@
-// Company Tags: Amazon, Adobe, Microsoft, Morgan Stanley, Samsung, Snapdeal, Times Internet
-
-// Go wherever the unsorted subarray/half because the minimum element is always there
 class Solution {
 public:
-    int findMin(vector<int>& nums) {
-        int n = nums.size();
-        int left = 0;
-        int right = n - 1;
-
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] > nums[right]) { // go right
-                left = mid + 1;  // no need to include mid
-            } else {
-                right = mid;  // include mid
-            }
-        }
-
-        return nums[right];
+   int findMin(vector<int>& arr) {
+    int n = arr.size(); // size of the array.
+    int mini = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        // Always keep the minimum.
+        mini = min(mini, arr[i]);
     }
+    return mini;
+}
+
 };
