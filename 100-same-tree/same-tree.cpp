3259cd134cc9f9206403e31a92b204e945d2b7d2
Ayush Gupta <92868752+ayushgupta9906@users.noreply.github.com>
2024-07-14 18:42:56 +0530

    Time: 6 ms (24.96%) | Memory: 12.2 MB (13.47%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p && !q)    return true;  // if one ends early then other, then this subtree not balanced here
-        if(!p || !q)    return false; // both ends together, the subtree is still same
-
-        // if value of current nodes not same, current subtree not same, no need to check left and right subtree and straighaway return false (trees not same)
-        return p->val==q->val 
-                    && isSameTree(p->left, q->left) 
-                    && isSameTree(p->right, q->right);
+        if (p == NULL && q == NULL) return true;
+        if (p == NULL || q == NULL) return false;
+        if (p->val != q->val) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
