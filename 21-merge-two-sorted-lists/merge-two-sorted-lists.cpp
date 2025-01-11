cc05f4619c6b2e6c8ce11104d4f59294c1eebba6
Ayush Gupta <92868752+ayushgupta9906@users.noreply.github.com>
2025-01-11 16:19:47 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (47.80%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* ptr1 = list1;
+        ListNode* ptr2 = list2;
+        if(ptr1 == NULL){
+            return list2;
+        }
+        if(ptr2 == NULL){
+            return list1;
+        }
+        if(ptr1->val < ptr2->val){
+            ptr1->next = mergeTwoLists(ptr1->next, ptr2);
+            return ptr1;
+        }
+        else{
+            ptr2->next = mergeTwoLists(ptr1, ptr2->next);
+            return ptr2;
+        }
+    }
+};
