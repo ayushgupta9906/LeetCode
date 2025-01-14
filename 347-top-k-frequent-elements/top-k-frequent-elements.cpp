a5c1bacbbe772667c4ea079d3a0440bc9aaed294
Ayush Gupta <92868752+ayushgupta9906@users.noreply.github.com>
2025-01-15 02:07:04 +0530

    Time: 3 ms (57.38%) | Memory: 20 MB (5.58%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& v, int k) {
-        int n=v.size();
-        vector<int>ans;
-        multiset<pair<int,int>,greater<>>ms;
-        map<int,int>m;
-        for(auto it:v) m[it]++;
-        for(auto it:m){
-            int a=it.second;
-            int b=it.first;
-            ms.insert({a,b});
-        }
-        while(k--){
-            auto p=*ms.begin();
-            ms.erase(ms.begin());
-            ans.push_back(p.second);
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int> map;
+        vector<int> ans;
+        for (int &x : nums) map[x]++;
+        vector<vector<int>> arr(n + 1);
+        for (auto [a, b] : map) arr[b].push_back(a);
+        for (int i = n; i > 0; i--) {
+            for (int &x : arr[i]) {
+                if (ans.size() == k) return ans;
+                ans.push_back(x);
+            }
         }
         return ans;
     }
