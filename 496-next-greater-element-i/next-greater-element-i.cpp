5a77b8d14c9169fdab875671f264572baded9643
Ayush Gupta <92868752+ayushgupta9906@users.noreply.github.com>
2025-09-05 21:04:23 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (91.38%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) 
-    {
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+
+        vector<int>result;
+        for(int x:nums1){
+            bool found=false;
+            int ans=-1;
+        for(int i=0;nums2.size();i++){
+            if(nums2[i]==x){
+                for(int j=i+1;j<nums2.size();j++){
+                    if(nums2[j]>x){
+                        ans=nums2[j];
+                        found=true;
+                        break;
+                    }
+                }
+                break;
+
+            }
+        }
+        result.push_back(ans);
+        }
+        return result;
+
+
         
-        vector<int> res(nums1.size(), -1); 
-          map<int,int>m1; 
-    stack<int>st;
-     int n=nums2.size();
-    
-  for(int i=n-1;i>=0;i--){
-      
-      while(!st.empty() && st.top()<nums2[i]){
-          st.pop();
-      }
-
-      if(st.empty()){
-          m1[nums2[i]]=-1;
-      }
-      
-      else{
-          m1[nums2[i]]=st.top();
-      }
-      
-      st.push(nums2[i]);
-
-      
-  }
-    vector<int>ans;
-    
-    for(auto i:nums1){
-        ans.push_back(m1[i]);
     }
-    
-    
-    return ans;
-}
 };
