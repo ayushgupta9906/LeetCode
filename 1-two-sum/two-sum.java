efeb443c60eae5ee5dfc8378c433a2fbf44afe4d
Ayush Gupta <92868752+ayushgupta9906@users.noreply.github.com>
2026-08-07 21:55:23 +0530

    Time: 8 ms (46.65%) | Memory: 47 MB (74.66%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,15 +1,38 @@
+import java.util.Arrays;
+
 class Solution {
+    
+    static class Pair{
+        int value;
+        int index;
+        Pair(int value,int index){
+            this.value=value;
+            this.index=index;
+        }
+    }
+
     public int[] twoSum(int[] nums, int target) {
         int n=nums.length;
+        Pair[]arr=new Pair[n];
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-
-                if(nums[i]+nums[j]==target){
-                    return new int[]{i,j};
-                }
-            }
+            arr[i]=new Pair(nums[i],i);
         }
-        return new int[]{};
+        Arrays.sort(arr,(a,b)->a.value-b.value);
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            int sum=arr[left].value+arr[right].value;
+            if(sum==target){
+                return new int[]{arr[left].index,arr[right].index};}
+                if(sum<target){
+                    left++;
+
+                }else{right--;
+                }
+
+            }
+            return new int[]{};
+            
+    }
         
     }
-}
