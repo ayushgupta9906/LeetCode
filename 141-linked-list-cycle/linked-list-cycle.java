b820e0ae63ded7a901250b67f0ce85d4588ab971
Ayush Gupta <92868752+ayushgupta9906@users.noreply.github.com>
2026-08-10 20:46:52 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (73.32%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -2,11 +2,8 @@ public class Solution {
 
     public boolean hasCycle(ListNode head) {
 
-        // Slow moves 1 step, fast moves 2 steps
         ListNode slow = head;
         ListNode fast = head;
-
-        // Check if fast can move safely
         while (fast != null && fast.next != null) {
 
             slow = slow.next;
