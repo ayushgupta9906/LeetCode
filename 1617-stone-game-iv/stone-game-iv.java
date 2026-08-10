b069933992ef983b351f3b11898b4aaa2c6f1eb2
Ayush Gupta <92868752+ayushgupta9906@users.noreply.github.com>
2026-08-10 18:20:13 +0530

    Time: 3 ms (99.74%) | Memory: 42.3 MB (90.34%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -5,17 +5,13 @@ class Solution {
 
         for (int i = 0; i <= n; i++) {
 
-            // If i is a losing position,
-            // every valid square move from i
-            // creates a winning position.
+     
             if (!dp[i]) {
 
                 for (int j = 1; i + j * j <= n; j++) {
                     dp[i + j * j] = true;
                 }
 
-                // Once n becomes reachable from a losing position,
-                // the first player can force a win.
                 if (dp[n]) {
                     return true;
                 }
